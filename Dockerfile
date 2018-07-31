7db5b94c1763f7d8435f904eedcf3d7cce45c2b6
Jonathan Senecal <jsenecal@connectitnet.com>
2018-07-31 15:59:54 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM ubuntu:latest
+
+LABEL maintainer="JSenecal@connectitnet.com"
+
+ENV DEBIAN_FRONTEND=noninteractive
+RUN apt-get update \
+ && apt-get install -y freeipa-client pwgen \
+ && rm -rf /var/lib/apt/lists/*
+
+# Remove NSSdb certificate repository contents
+RUN rm -rfv /etc/pki/nssdb
+
+ADD docker-entrypoint.sh /
+RUN chmod +x /docker-entrypoint.sh
+
+VOLUME [ "/etc/pki/nssdb" ]
+VOLUME [ "/etc/ipa" ]
+
+ENV NSSDB_DIR=/etc/pki/nssdb
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -eo pipefail
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+: ${NSS_PASSWD_FILE:-"/root/.nsspass"}
+if [ ! -d "$NSSDB_DIR" ]; then
+    # Create NSSdb directory
+    mkdir -p $NSSDB_DIR
+    if [ ! -f "$NSS_PASSWD_FILE" ]; then
+        # Generate NSS pass
+        /usr/bin/pwgen 16 1 > $NSS_PASSWD_FILE
+    fi
+    # Create new NSSdb certificate repository
+    /usr/bin/certutil -N -d $NSSDB_DIR -f $NSS_PASSWD_FILE
+fi
+
+if [ ! -f /etc/ipa/ca.crt ]; then
+    file_env 'IPA_PASSWORD'
+    if [[ -z "$IPA_PASSWORD" && -z "$IPA_DOMAIN" && -z "$IPA_PRINCIPAL" ]]; then
+        echo >&2 'error: IPA client is not configured and "IPA_*" settings are not specified'
+        exit 1
+    fi
+    # Install ipa client
+    /usr/sbin/ipa-client-install -U -f --no-ntp --mkhomedir --domain=$IPA_DOMAIN -w $IPA_PASSWORD -p $IPA_PRINCIPAL
+fi
+
