f25ee672e1db04ca7470e8884c95b744d30aedfb
Jonathan Senecal <jsenecal@connectitnet.com>
2018-08-01 13:28:33 -0400

    Handle already installed client exit code

--- src/etc/my_init.d/ipa-client-install.sh
+++ src/etc/my_init.d/ipa-client-install.sh
@@ -36,7 +36,7 @@ if [ ! -d "$NSSDB_DIR" ]; then
     /usr/bin/certutil -N -d $NSSDB_DIR -f $NSS_PASSWD_FILE
 fi
 
-if [[ ! -f /etc/ipa/ca.crt || ! -f /etc/openldap/ldap.conf ]]; then
+if [[ ! -f "/etc/ipa/ca.crt" || ! -f "/etc/openldap/ldap.conf" ]]; then
     file_env 'IPA_PASSWORD'
     if [[ -z "$IPA_PASSWORD" && -z "$IPA_DOMAIN" && -z "$IPA_PRINCIPAL" ]]; then
         echo >&2 'error: IPA client is not configured and "IPA_*" settings are not specified'
@@ -44,4 +44,9 @@ if [[ ! -f /etc/ipa/ca.crt || ! -f /etc/openldap/ldap.conf ]]; then
     fi
     # Install ipa client
     /usr/sbin/ipa-client-install -U -f --no-ntp --mkhomedir --domain=$IPA_DOMAIN -w $IPA_PASSWORD -p $IPA_PRINCIPAL < /dev/null
+	if [ $? -eq 3 ]; then
+		exit 0
+	else
+		echo "ipa-client-install failed with status $?" >&2
+	fi
 fi
