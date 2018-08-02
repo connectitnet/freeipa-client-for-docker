1fe7e385a89dd032ef9fb40d919b26067e651168
Jonathan Senecal <jsenecal@connectitnet.com>
2018-08-02 11:06:22 -0400

    Better handling of volumes and startup process

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ LABEL maintainer="JSenecal@connectitnet.com"
 
 ENV DEBIAN_FRONTEND=noninteractive
 RUN install_clean freeipa-client pwgen \
- && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+ && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
+ && mv /etc/krb5.conf /etc/ipa
 
 # Remove NSSdb certificate repository contents
 RUN rm -rfv /etc/pki/nssdb
@@ -13,6 +14,6 @@ ADD /src /
 RUN chmod +x /etc/service/*/run
 RUN chmod +x /etc/my_init.d/*.sh
 
-VOLUME [ "/etc" ]
+VOLUME ["/etc/ipa", "/etc/sssd", "/etc/pki", "/etc/openldap"]
 
 CMD ["/sbin/my_init"]
--- src/etc/my_init.d/ipa-client-install.sh
+++ src/etc/my_init.d/ipa-client-install.sh
@@ -23,6 +23,11 @@ file_env() {
 	unset "$fileVar"
 }
 
+if [[ ! -f "/etc/krb5.conf" ]]; then
+	# Make sure the Kerberos settings survive a container restart
+	ln -s /etc/ipa/krb5.conf /etc/krb5.conf
+fi
+
 # Generate default NSS Password
 DEF_NSS_PASSWD=$(/usr/bin/pwgen 16 1)
 file_env 'NSS_PASSWD' $DEF_NSS_PASSWD
@@ -36,17 +41,30 @@ if [ ! -d "$NSSDB_DIR" ]; then
     /usr/bin/certutil -N -d $NSSDB_DIR -f $NSS_PASSWD_FILE
 fi
 
-if [[ ! -f "/etc/ipa/ca.crt" || ! -f "/etc/openldap/ldap.conf" ]]; then
+if [ ! -f /etc/ipa/ca.crt ]; then
     file_env 'IPA_PASSWORD'
     if [[ -z "$IPA_PASSWORD" && -z "$IPA_DOMAIN" && -z "$IPA_PRINCIPAL" ]]; then
         echo >&2 'error: IPA client is not configured and "IPA_*" settings are not specified'
         exit 1
     fi
     # Install ipa client
-    /usr/sbin/ipa-client-install -U -f --no-ntp --mkhomedir --domain=$IPA_DOMAIN -w $IPA_PASSWORD -p $IPA_PRINCIPAL < /dev/null
-	if [ $? -eq 3 ]; then
+    /usr/sbin/ipa-client-install -U -f --no-ntp --mkhomedir --domain=$IPA_DOMAIN -w $IPA_PASSWORD -p $IPA_PRINCIPAL --enable-dns-updates --no-nisdomain < /dev/null
+	exit_status=$?
+	cp -f /etc/hostname /etc/ipa/hostname
+	if [ $exit_status -eq 3 ]; then
+		# Do not crash if already installed
 		exit 0
-	else
-		echo "ipa-client-install failed with status $?" >&2
 	fi
+	exit $exit_status
+else
+	IPA_HOSTNAME=$(cat /etc/ipa/hostname)
+	if [ ! "$IPA_HOSTNAME" == "$(hostname -f)" ] ; then
+		if hostname $IPA_HOSTNAME ; then
+			echo "Hostname set to $IPA_HOSTNAME"
+		else
+			echo "The container hostname is $(hostname -f) and cannot set $IPA_HOSTNAME; run with -h." >&2
+			exit 1
+		fi
+	fi
+	echo "System is already FreeIPA-enrolled, moving on..."
 fi
